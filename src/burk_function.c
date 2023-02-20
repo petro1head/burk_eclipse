7cedef22bd03e044c24afe076c45d1660d191770
petro1head <makeeva.1999ghbhjlf@gmail.com>
2023-02-20 22:37:31 +0300

    Парсинг в одну строку и строка bad

--- src/burk_function.c
+++ src/burk_function.c
@@ -1,9 +1,7 @@
 #include "burk_function.h"
 
-
 //default setting function
-void set_default (MySystem *ps)
-{
+void set_default(MySystem *ps) {
 	ps->integ.s = 0;
 	ps->integ.h = 0;
 	ps->integ.t_before = 0;
@@ -24,200 +22,187 @@ void set_default (MySystem *ps)
 	ps->ft.cnt_start = 0;
 	ps->ft.is_engine_work = 0;
 	ps->ft.t_off = 0;
-	ps->ft.t_on= 0;
-	ps->ft.u=0;
+	ps->ft.t_on = 0;
+	ps->ft.u = 0;
 }
 
-
 // интегратор (угол)
-void integratorIn(MySystem *ps, double in_u, unsigned long t)
-{
-  ps->integ.h = t - ps->integ.t_before;
-  ps->integ.s += in_u * ps->integ.h * 0.001;
-  ps->integ.t_before = t;
+void integratorIn(MySystem *ps, double in_u, unsigned long t) {
+	ps->integ.h = t - ps->integ.t_before;
+	ps->integ.s += in_u * ps->integ.h * 0.001;
+	ps->integ.t_before = t;
 }
 
 /* Дискретный интегратор */
 /* Подаём на вход сигнал */
 // in_u - значение сигнала на входе в интегратор
 // t - текущее время
-void disc_integratorIn(MySystem *ps, double in_u, unsigned long t)
-{
+void disc_integratorIn(MySystem *ps, double in_u, unsigned long t) {
 
-  // Параметры дискретного интегратора
-  // k - коэффициент
-  double K = 1;
-  // Шаг дискретизации
-  unsigned long T = 10;
+	// Параметры дискретного интегратора
+	// k - коэффициент
+	double K = 1;
+	// Шаг дискретизации
+	unsigned long T = 10;
 
-  // Если мы ждали больше шага дискретизации
-  if (t - ps->disc_integrator.t_before >= T) {
-    // Обновляем значение интеграла
-	  ps->disc_integrator.u += in_u * K * T * 0.001;  // мс
-    // Обновляем сохранённое время
-	  ps->disc_integrator.t_before = t;
-  }
-  // Если еще не прождали нужный шаг дискретизации
-  // То ничего не делаем, и выводим предыдущий результат
-  // выход
+	// Если мы ждали больше шага дискретизации
+	if (t - ps->disc_integrator.t_before >= T) {
+		// Обновляем значение интеграла
+		ps->disc_integrator.u += in_u * K * T * 0.001;  // мс
+		// Обновляем сохранённое время
+		ps->disc_integrator.t_before = t;
+	}
+	// Если еще не прождали нужный шаг дискретизации
+	// То ничего не делаем, и выводим предыдущий результат
+	// выход
 }
 
 // Summator
-void summatorIn2(MySystem *ps, double u1, double u2)
-{
-  // Значение сигнала, полученного Сумматором
-  ps->sum2.u = u1 + u2;
+void summatorIn2(MySystem *ps, double u1, double u2) {
+	// Значение сигнала, полученного Сумматором
+	ps->sum2.u = u1 + u2;
 }
 
-
-void summatorIn3(MySystem *ps, double u1, double u2, double u3)
-{
-  // Значение сигнала, полученного Сумматором
+void summatorIn3(MySystem *ps, double u1, double u2, double u3) {
+	// Значение сигнала, полученного Сумматором
 	ps->sum3.u = u1 + u2 + u3;
 }
 
-
 // Gain
-void gainIn(MySystem *ps, double in_u)
-{
-  double multiplier = 0.1;
-  ps->gain.u = in_u * multiplier;
+void gainIn(MySystem *ps, double in_u) {
+	double multiplier = 0.1;
+	ps->gain.u = in_u * multiplier;
 }
 
-
 // Saturation
-void saturationIn(MySystem *ps, double in_u)
-{
-  // Верхняя граница сигнала
-  double upper = 0.25;
-  // Нижняя граница сигнала
-  double lower = -0.25;
+void saturationIn(MySystem *ps, double in_u) {
+	// Верхняя граница сигнала
+	double upper = 0.25;
+	// Нижняя граница сигнала
+	double lower = -0.25;
 
-  if (in_u >= upper) {
-    ps->sat.u = upper;
-  } else if (in_u <= lower) {
-	  ps->sat.u = lower;
-  } else {
-	  ps->sat.u = in_u;
-  }
+	if (in_u >= upper) {
+		ps->sat.u = upper;
+	} else if (in_u <= lower) {
+		ps->sat.u = lower;
+	} else {
+		ps->sat.u = in_u;
+	}
 }
 
 // Relay
-void relayIn(MySystem *ps, double in_u, double sp, double an)
-{
+void relayIn(MySystem *ps, double in_u, double sp, double an) {
 
-  // Порог срабатывания реле
-  double positive = 0.125;
-  double negative = -0.125;
+	// Порог срабатывания реле
+	double positive = 0.125;
+	double negative = -0.125;
 
-  if (sp < 0.08 && an < 0.4) {
-    positive = 0.03;
-    negative = -0.03;
-  }
+	if (sp < 0.08 && an < 0.4) {
+		positive = 0.03;
+		negative = -0.03;
+	}
 
-  // Преобразуем сигнал либо к -1, либо 0, либо 1
-  if (in_u >= positive) {
-    ps->relay.u = 1;
-  } else if (in_u <= negative) {
-	 ps->relay.u = -1;
-  } else {
-    // u принадлежит интервалу (negative, positive )
-	 ps->relay.u = 0;
-  }
+	// Преобразуем сигнал либо к -1, либо 0, либо 1
+	if (in_u >= positive) {
+		ps->relay.u = 1;
+	} else if (in_u <= negative) {
+		ps->relay.u = -1;
+	} else {
+		// u принадлежит интервалу (negative, positive )
+		ps->relay.u = 0;
+	}
 }
 
 // Regulator
 
-void regulatorIn(MySystem *ps, unsigned long t, double speed)
-{
-  integratorIn (ps, speed, t);
-  summatorIn2(ps, -ps->integ.s, -ps->disc_integrator.u);
-  gainIn(ps, ps->sum2.u);
-  saturationIn(ps, ps->gain.u);
-  summatorIn3(ps, -speed, ps->sat.u, -ps->relay.u);
-  disc_integratorIn(ps, ps->sum3.u, t);
-  relayIn(ps, ps->disc_integrator.u, speed, ps->integ.s);
+void regulatorIn(MySystem *ps, unsigned long t, double speed) {
+	integratorIn(ps, speed, t);
+	summatorIn2(ps, -ps->integ.s, -ps->disc_integrator.u);
+	gainIn(ps, ps->sum2.u);
+	saturationIn(ps, ps->gain.u);
+	summatorIn3(ps, -speed, ps->sat.u, -ps->relay.u);
+	disc_integratorIn(ps, ps->sum3.u, t);
+	relayIn(ps, ps->disc_integrator.u, speed, ps->integ.s);
 }
 
 /* Ф от тау */
 // F_ot_tau
 //  tau - задержку на включение двигателя в [мс]
-void f_ot_tauIn(MySystem *ps, unsigned long t, double in_u)
-{
-  // Параметр тау
-  unsigned long tau = 30;
+void f_ot_tauIn(MySystem *ps, unsigned long t, double in_u) {
+	// Параметр тау
+	unsigned long tau = 30;
 
-  // Проверяем работает ли двигатель
-  // Если работает
-  if (ps->ft.is_engine_work) {
-    // Находим время работы двигателя
-    // Это текущее время минус время запуска двигателя
-    unsigned long wake_time = t - ps->ft.t_on;
-    // Если  время работы двигателя >30мс
-    if (wake_time > tau) {
-      // Нужно проверить значение сигнала
-      // Если значение сигнала не равно сигналу при включении двигателя
-      if (in_u != ps->ft.u) {
-        // Нужно выключить двигатель
-    	  ps->ft.is_engine_work = 0;
-        // Задаём время выключения двигателя - теущее
-    	  ps->ft.t_off = t;
-        // сохранить значение сигнала, ставим именно 0
-        // Потому что у нас ступенчатый вид сигнала
-        // И должна быть его 0 фаза
-        // Не может быть чтобы сигнал был,
-        // например 0.5 и сразу стал -0.5
-    	  ps->ft.u = 0;
-      }
-    }
-  } else {
-    // Двигатель не работает
-    // Проверяем запускался ли он хоть раз
-    // Если еще не запускался
-    if (ps->ft.cnt_start == 0) {
-      // То нам не нужно вычислять время спячки двигателя
-      // И как только сигнал будет не нулевой
-      // Включаем двигатель
-      // если текущее значение сигнала НЕ нулевое
-      if (in_u != 0) {
-        // Включаем двигатель
-    	  ps->ft.is_engine_work = 1;
-        // Увеличиваем счётчик запусков двигателей
-    	  ps->ft.cnt_start += 1;
-        // сохраняем время запуска двигателя - текущее время
-    	  ps->ft.t_on = t;
-        // сохранить значение сигнала двигателя при старте
-        // - то есть текущее значенеи сигнала
-    	  ps->ft.u = in_u;
-      }
-    } else {
-      // Двигатель УЖЕ запускался
-      // Нужно проверить значение сигнала
-      // Если сигнал НЕ нулевой
-      if (in_u != 0) {
-        // Включаем двигатель
-    	  ps->ft.is_engine_work = 1;
-        // Увеличиваем счётчик запусков двигателей
-    	  ps->ft.cnt_start += 1;
-        // сохраняем время запуска двигателя - текущее время
-    	  ps->ft.t_on = t;
-        // сохранить значение сигнала двигателя при старте
-        // - то есть текущее значение сигнала
-    	  ps->ft.u = in_u;
-      }
-    }
-  }
+	// Проверяем работает ли двигатель
+	// Если работает
+	if (ps->ft.is_engine_work) {
+		// Находим время работы двигателя
+		// Это текущее время минус время запуска двигателя
+		unsigned long wake_time = t - ps->ft.t_on;
+		// Если  время работы двигателя >30мс
+		if (wake_time > tau) {
+			// Нужно проверить значение сигнала
+			// Если значение сигнала не равно сигналу при включении двигателя
+			if (in_u != ps->ft.u) {
+				// Нужно выключить двигатель
+				ps->ft.is_engine_work = 0;
+				// Задаём время выключения двигателя - теущее
+				ps->ft.t_off = t;
+				// сохранить значение сигнала, ставим именно 0
+				// Потому что у нас ступенчатый вид сигнала
+				// И должна быть его 0 фаза
+				// Не может быть чтобы сигнал был,
+				// например 0.5 и сразу стал -0.5
+				ps->ft.u = 0;
+			}
+		}
+	} else {
+		// Двигатель не работает
+		// Проверяем запускался ли он хоть раз
+		// Если еще не запускался
+		if (ps->ft.cnt_start == 0) {
+			// То нам не нужно вычислять время спячки двигателя
+			// И как только сигнал будет не нулевой
+			// Включаем двигатель
+			// если текущее значение сигнала НЕ нулевое
+			if (in_u != 0) {
+				// Включаем двигатель
+				ps->ft.is_engine_work = 1;
+				// Увеличиваем счётчик запусков двигателей
+				ps->ft.cnt_start += 1;
+				// сохраняем время запуска двигателя - текущее время
+				ps->ft.t_on = t;
+				// сохранить значение сигнала двигателя при старте
+				// - то есть текущее значенеи сигнала
+				ps->ft.u = in_u;
+			}
+		} else {
+			// Двигатель УЖЕ запускался
+			// Нужно проверить значение сигнала
+			// Если сигнал НЕ нулевой
+			if (in_u != 0) {
+				// Включаем двигатель
+				ps->ft.is_engine_work = 1;
+				// Увеличиваем счётчик запусков двигателей
+				ps->ft.cnt_start += 1;
+				// сохраняем время запуска двигателя - текущее время
+				ps->ft.t_on = t;
+				// сохранить значение сигнала двигателя при старте
+				// - то есть текущее значение сигнала
+				ps->ft.u = in_u;
+			}
+		}
+	}
 }
 
-void SystemRun(MySystem *ps, unsigned long t, double speed) {  // Делаем прогон через систему
-  // Передаём на вход регулятора значение времени, скорости и угла
-  regulatorIn(ps, t, speed);
-  // В Ф(тау) передаём текущее время и выхлоп регулятора
-  f_ot_tauIn(ps, t, ps->relay.u);
+void SystemRun(MySystem *ps, unsigned long t, double speed) { // Делаем прогон через систему
+	// Передаём на вход регулятора значение времени, скорости и угла
+	regulatorIn(ps, t, speed);
+	// В Ф(тау) передаём текущее время и выхлоп регулятора
+	f_ot_tauIn(ps, t, ps->relay.u);
 }
 // Что получаем на выходе из системы
 double SystemOut(MySystem *ps) {
-  // Система заканчивается Ф от тау
-  return ps->ft.u;
+	// Система заканчивается Ф от тау
+	return ps->ft.u;
 }
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
-
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "uart.h"
 #include "chip_vm014.h"
 #include "devdescr.h"
@@ -8,61 +8,57 @@
 #include "io.h"
 #include "burk_function.h"
 
-
-int _getpid_r(struct _reent * a) {
+int _getpid_r(struct _reent *a) {
 	return 0;
 }
 
-int _kill_r(struct _reent * a, int b, int c) {
+int _kill_r(struct _reent *a, int b, int c) {
 	return 0;
 }
 
-_PTR _calloc_r(struct _reent * a, size_t b, size_t c)
-{
+_PTR _calloc_r(struct _reent *a, size_t b, size_t c) {
 	return 0;
 }
-
 
 typedef struct Buffer {
 	char str[100];
 	int len;
 } Buffer;
 
-
-int parse(unsigned long * t, double *speed, char data[100])
-{
-	char * sep = NULL;
-	char t_str[100];
-	char speed_str[100];
-
-	sep = strchr(data, ';');
-
-	if (sep != NULL)
-	{
-		strncpy(t_str, data, (sep - data) / sizeof(char));
-		*t = atoi(t_str);
-		strcpy(speed_str, (sep + 1));
-		*speed = atof(speed_str);
-		return 1;
-	}
-
-	return 0;
-}
-
+//int parse(unsigned long *t, double *speed, char data[100]) {
+//	char *sep = NULL;
+//	char t_str[100];
+//	char speed_str[100];
+//
+//	sep = strchr(data, ';');
+//
+//	if (sep != NULL) {
+//		strncpy(t_str, data, (sep - data) / sizeof(char));
+//		*t = atoi(t_str);
+//		strcpy(speed_str, (sep + 1));
+//		*speed = atof(speed_str);
+//		return 1;
+//	}
+//
+//	return 0;
+//}
 
 void outPrintln(MySystem *ps) {
- // Serial.println(SystemOut());
-	uart_send_number(0, SystemOut(ps));
+	// Serial.println(SystemOut());
+	//uart_send_number(0, SystemOut(ps));
+	char string [10];
+	sprintf (string, "%lf.3\n",SystemOut(ps));
+	uart_send_string(0, (unsigned char*) string);
 }
 
 int main(void) {
 
-	volatile UART_UNIT * uart0 = (UART_UNIT*) 0xBFF00100;
-	uart0 -> IIRF = 0xff; //fifo
-	uart0 -> IIRF = 0xff;
+	volatile UART_UNIT *uart0 = (UART_UNIT*) 0xBFF00100;
+	uart0->IIRF = 0xff; //fifo
+	uart0->IIRF = 0xff;
 
 	uart_baud_set(0, 26); // baud 57600 bit/s
-	uart0 -> LCR = 3; //  8 bit data, no parity, 1 stop bit // uart1->LCR = (1<<0)|(1<<1)
+	uart0->LCR = 3; //  8 bit data, no parity, 1 stop bit // uart1->LCR = (1<<0)|(1<<1)
 
 	unsigned long t = 0;
 	double speed = 0;
@@ -77,7 +73,7 @@ int main(void) {
 	MySystem s;
 	set_default(&s);
 
-	uart_send_string(0, (unsigned char *)"OK");
+	uart_send_string(0, (unsigned char*) "OK");
 
 	while (1) {
 		if (uart_check_in(0)) {
@@ -101,20 +97,38 @@ int main(void) {
 			}
 		}
 
-		if (readed_data) {
+		if (readed_data)
+		{
 			// down flag for new read data
 			readed_data = 0;
 			// parse data
-			if (parse(&t, &speed, qt_data))
+	//			if (parse(&t, &speed, qt_data))
+	//			{
+	//				// system process run
+	//				SystemRun(&s, t, speed);
+	//				outPrintln(&s);
+	//
+	//			}
+			if (sscanf(qt_data, "%lu;%lf", &t, &speed))
 			{
 				// system process run
 				SystemRun(&s, t, speed);
 				outPrintln(&s);
+			}
 
-			}else {
+			else
+			{
+				//char bad_string[100] = "bad";
+				char bad_string[100];
 
-				//send old data
-			     }
+				// bad string
+				//sprintf (bad_string, "%s\n", qt_data);
+     			//strcat(bad_string, qt_data);
+				//strcat(bad_string, "\n");
+
+				sprintf (bad_string, "bad%s\n", qt_data);
+				uart_send_string(0, (unsigned char*)bad_string);
+			}
 		}
 
 	}
